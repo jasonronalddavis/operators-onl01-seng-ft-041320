599cf0032c34293db07e2c8418f21d8ca9dd908a
Jason Davis <jdavis@c.ringling.edu>
2020-04-20 23:40:31 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -7,11 +7,9 @@ end
 
 
 
-
-
 def not_safe?(speed)
 speed>60 ? true : speed<60
-speed>40 ? true : speed<40
+speed<40 ? true : speed>40
 speed<=60 ? false : speed>=60
 speed<=40 ? false : speed>=40
 end
