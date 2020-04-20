8df33c30e64e5871d0290ae125b49fc5e63c1f10
Jason Davis <jdavis@c.ringling.edu>
2020-04-20 23:23:17 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,7 +3,10 @@ return true if speed>60 unless speed<60
 return true if speed<40 unless speed>40
 return false if speed<=60 unless speed>=60
 return false if speed<=40 unless speed>=40
+
+
 end
+
 
 
 
