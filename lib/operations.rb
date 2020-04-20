60430e9f13eb713bd2ad45760516754b9a6839e5
Jason Davis <jdavis@c.ringling.edu>
2020-04-20 23:31:59 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -14,3 +14,10 @@ return true if speed<40 unless speed>40
 return false if speed<=60 unless speed>=60
 return false if speed<=40 unless speed>=40
 end
+
+def not_safe?(speed)
+speed>60 ? true : speed<60
+return true if speed<40 unless speed>40
+return false if speed<=60 unless speed>=60
+return false if speed<=40 unless speed>=40
+end
